b97623e8082eedd7138a714d3e4572e2f5a06eff
Patricia De Carli <patricia.decarli@vialink.fr>
2023-04-12 10:38:02 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText: string = lorem.sentence();
+  enteredText: string = '';
+  
+  randomTextPhrases = this.randomText.split(" ");
+  randomTextLength = this.randomTextPhrases.length;
+  enteredTextPhrases = this.enteredText.split(" ");
+
+  solved: boolean = false;
+
+  onInput(value: string) {
+    value === this.randomText ? this.solved = true : this.solved = false;
+    this.enteredTextPhrases = value.split(" ");
+    
+    this.enteredText = value;
+    this.randomTextPhrases.map(word => {
+      for (let i = 0; i < this.enteredTextPhrases.length; i++) {
+        const element = this.enteredTextPhrases[i];
+        
+      }
+    })
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if(!enteredLetter) {
+      return 'pending'
+    }
+
+    return enteredLetter === randomLetter ? 'correct' : 'incorrect';
+  }
+
+
 }
